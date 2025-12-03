280db99d2514a2c63e646095102ae3ec33d4afbe
A1ex <a1ex@a1ex.fr>
2025-12-03 01:30:31 -0500

    feat: Day03

---
+++ Day03/part2.mjs
@@ -0,0 +1,29 @@
+import fs from "node:fs";
+
+const input = fs.readFileSync("./input").toString();
+const lines = input.split("\n").filter(a => a).map((line) => line.split(''))
+let result = 0;
+
+const DIGITS = 12;
+
+const findHighestNumber = (array) => {
+    const deepArray = JSON.parse(JSON.stringify(array))
+    deepArray.sort((a, b) => b - a)
+    const number = deepArray[0];
+    const index = array.findIndex((n) => n === number)
+    return { number, index }
+}
+
+lines.forEach(line => {
+    let joltage = ""
+    for (let i = DIGITS - 1; i >= 0; i--) {
+        const lineCopy = JSON.parse(JSON.stringify(line))
+        if (i !== 0) lineCopy.splice(-i)
+        const highestNumber = findHighestNumber(lineCopy)
+        line = line.slice(highestNumber.index + 1)
+        joltage += highestNumber.number
+    }
+    result += Number(joltage)
+})
+
+console.log(result)
